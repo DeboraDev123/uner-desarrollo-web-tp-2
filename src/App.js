648c31e65e4c6fbeda55ad9602dbba73f5370a3f
Nahir-Orellana-Lopez <nahir.orellana.lopez@gmail.com>
2024-06-24 13:14:01 -0300

    Se removió Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import "./normalize.css";
-
-import { Switch, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
-import Footer from "./components/Footer";
-import Components from "./pages/Components";
-import Alojamiento from "./pages/alojamiento/Alojamiento";
-import { routes } from "./routes/routes";
+import './normalize.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import Footer from './components/Footer';
+import Alojamiento from './pages/alojamiento/Alojamiento';
+import { routes } from './routes/routes';
 
 function App() {
   return (
-    <div className="App" data-mobile-nav-link="hidden">
+    <div className='App' data-mobile-nav-link='hidden'>
       <Navbar></Navbar>
 
       <Switch>
@@ -27,22 +24,18 @@ function App() {
             </Route>
           );
         })}
-        <Route path="/alojamiento/:id">
+        <Route path='/alojamiento/:id'>
           <Alojamiento />
         </Route>
         {/* DE MOMENTO LOS 404 REDIRECCIONAN AL HOME */}
-        <Route path="/components">
-          <Components />
-        </Route>
-        <Route path="*">
-          <Redirect to="/" />
+        <Route path='*'>
+          <Redirect to='/' />
         </Route>
         {/* DE MOMENTO LOS 404 REDIRECCIONAN AL HOME */}
       </Switch>
       <Footer>
-      <Link to="/contacto">Contacto</Link>
-      <Link to="/Acerca">Contacto</Link>
-
+        <Link to='/contacto'>Contacto</Link>
+        <Link to='/Acerca'>Contacto</Link>
       </Footer>
     </div>
   );
